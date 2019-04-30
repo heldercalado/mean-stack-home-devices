8532a7e938fb45e87ea5550f729cbd064983921f
Helder Calado <hbcalado@gmail.com>
2019-04-30 06:37:39 -0700

    added rating system and reviews quantity in consoles section

--- src/app/Components/consoles/consoles.component.ts
+++ src/app/Components/consoles/consoles.component.ts
@@ -2,16 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from 'src/app/Services/communication.service';
 import { ItemsService } from 'src/app/Services/items.service';
 import { Item } from '../../Interfaces/item';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-consoles',
   templateUrl: './consoles.component.html',
-  styleUrls: ['./consoles.component.css']
+  styleUrls: ['./consoles.component.css'],
+  styles: [`
+    .star {
+      font-size: 1.5rem;
+      color: #b0c4de;
+    }
+    .filled {
+      color: #daad86;
+    }
+    .bad {
+      color: #deb0b0;
+    }
+    .filled.bad {
+      color: #daad86;
+    }
+  `]
 })
 export class ConsolesComponent implements OnInit {
 pageName = 'Consoles';
 itemList: Item[] = [];
-  constructor(private comm: CommunicationService , private itemsService: ItemsService) { }
+  constructor(private comm: CommunicationService , private itemsService: ItemsService, private ratingConfig: NgbRatingConfig) {
+    ratingConfig.max = 5;
+    ratingConfig.readonly = true;
+   }
 
   ngOnInit() {
     this.getConsoleItems();
@@ -24,5 +43,11 @@ getConsoleItems() {
     this.itemList = data;
 
   });
+}
+getRandomRating(){
+  return Math.floor(Math.random() * Math.floor(this.ratingConfig.max));
+}
+getRandomReviewsNumber(){
+  return '(' + Math.floor(Math.random() * Math.floor(500)) + ')';
 }
 }
